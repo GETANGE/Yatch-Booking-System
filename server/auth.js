d572fc53fcb70ac9f2cd28cd1292144eb527e5ab
Getange <emmanuelgetange48@gmail.com>
2023-04-29 00:18:36 +0300

    enabling authenticaton

--- server/auth.js
+++ server/auth.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyD6kSccasMObAYaEjiGtqquhAgv3Z15xCM",
     authDomain: "contactform-yatch.firebaseapp.com",
@@ -9,8 +12,27 @@ const firebaseConfig = {
     measurementId: "G-RW4G51KSGH"
   }; 
   
-  //initialize firebase.
-  firebase.initializeApp(firebaseConfig);
-  //initialize variables.
-  const auth = firebase.auth();
-  const database = firebase.database();
+//initialize firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const loginForm = document.getElementById('login-form');
+
+
+loginForm.addEventListener('submit', (e)=>{
+ e.preventDefault(); //prevent default  
+ const email = loginForm.email.value;
+ const password = loginForm.password-input.value;
+
+
+ createUserWithEmailAndPassword(auth, email, password)
+ .then((userCredential) => {
+     //logged in
+     const user = userCredential.user;
+     console.log('User logged in',user);
+     //...
+ })
+ .catch((error) => {
+   console.log('User not logged in ',error);
+ });
+});
